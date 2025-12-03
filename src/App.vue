107ce1be8cb0c444a4dab676b69e62b7b4709dda
Mike Mayer <mymore4ok@gmail.com>
2025-12-04 00:45:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-2xl p-6">
+    Hello, {{ user?.first_name }}!
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const user = ref<any>(null);
+const theme = ref<any>(null);
+
+onMounted(() => {
+    window.Telegram?.WebApp?.ready();
+    user.value = window.Telegram?.WebApp?.initDataUnsafe?.user;
+    theme.value = window.Telegram?.WebApp?.themeParams;
+});
+</script>
